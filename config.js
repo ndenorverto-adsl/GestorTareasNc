41e3942b75196ee745db76072307e8cbff754522
ndenorverto-adsl <ndenorverto@gmail.com>
2026-09-03 16:57:00 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.NC_CFG={url:"",anon:"",email:""};
+window.NC_CFG={url:"https://jukxgminoatluaoqgbfa.supabase.co",anon:"sb_publishable_blxCjfEW2iFCaCmdmnRPsw_CZ5eei7Y",email:"nicoplas.dneorverto@adslhouse.com"};
